8b23c75665618c68f2ca8d4dd72efa168bc4e749
Callum Jobbins <callum.jobbins@bitce.co.uk>
2023-08-17 23:15:17 +0200

    watch list

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,7 @@
 class Bookmark < ApplicationRecord
-  belongs_to :Movie
-  belongs_to :List
+  belongs_to :movie
+  belongs_to :list
+
+  validates :comment, length: { minimum: 6 }
+  validates :movie, uniqueness: { scope: :list }
 end
