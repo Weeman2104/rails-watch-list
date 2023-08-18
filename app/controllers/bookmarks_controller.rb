cfc17494f1c75407fad92e74e16423bcac4030bb
Callum Jobbins <callum.jobbins@bitce.co.uk>
2023-08-18 09:19:24 +0200

    watch list

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -8,7 +8,7 @@ class BookmarksController < ApplicationController
     @list = List.find(params[:list_id])
     @bookmark = @list.bookmarks.build(bookmark_params)
     if @bookmark.save
-      redirect_to list_path(@list)
+      redirect_to @list
     else
       render :new, status: :unprocessable_entity
     end
@@ -16,8 +16,9 @@ class BookmarksController < ApplicationController
 
   def destroy
     @bookmark = Bookmark.find(params[:id])
+    list = @bookmark.list
     @bookmark.destroy
-    redirect_to list_path
+    redirect_to list
   end
 
   private
